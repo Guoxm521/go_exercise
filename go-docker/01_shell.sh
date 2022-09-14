034d5570f87e9ac7e3d6f6ac118ff4df869f0442
gxm <2925891606@qq.com>
2022-09-14 23:28:50 +0800

    add

--- go-docker/01_shell.sh
+++ go-docker/01_shell.sh
@@ -2,6 +2,11 @@
 
 echo "前段项目编译"
 cd "G:\document\documents"
-git pull origin main
+#git pull origin main
 npm run docs:build
+sleep 3s
+echo 1233123123
+sleep 3s
+echo 45454545
 
+#https://blog.csdn.net/nangonghen/article/details/107707725
--- go-docker/demo.go
+++ go-docker/demo.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"os"
 	"os/exec"
 )
@@ -12,7 +13,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"Message": "docker部署镜像你好呀",
+			"html": "<b>Hello, world!</b>",
 		})
 	})
 	r.GET("/go/ping", func(c *gin.Context) {
@@ -28,12 +29,43 @@ func main() {
 		}
 		cmd.Start()
 		in := bufio.NewScanner(stdout)
+		cmdRe := ""
 		for in.Scan() {
-			cmdRe := string(in.Bytes())
-			c.JSON(200, gin.H{
-				"Message": cmdRe,
-			})
+			cmdRe = string(in.Bytes())
 		}
+		c.JSON(200, gin.H{
+			"Message": cmdRe,
+		})
+	})
+	r.GET("/go2", func(c *gin.Context) {
+		w := c.Writer
+		header := w.Header()
+		//在响应头添加分块传输的头字段Transfer-Encoding: chunked
+		header.Set("Transfer-Encoding", "chunked")
+		header.Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		cmd := exec.Command("D:/BianCheng/Git/bin/bash", "./01_shell.sh")
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error=>", err.Error())
+		}
+		cmd.Start()
+		in := bufio.NewScanner(stdout)
+		for in.Scan() {
+			w.WriteString(in.Text() + "\n")
+			w.(http.Flusher).Flush()
+			fmt.Println(in.Text())
+			//c.Stream(func(w io.Writer) bool {
+			//	w.Write(in.Bytes())
+			//	w.Write([]byte{'\n'})
+			//	d.Flush()
+			//	return false
+			//})
+
+		}
+		w.(http.Flusher).Flush()
+		cmd.Wait()
 
 	})
 	r.Run()
